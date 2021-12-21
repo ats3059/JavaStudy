069321c3c3b4e9a1e780f3c4a413e6038c4ce722
ats3059 <ats3059@naver.com>
2021-12-21 23:47:32 +0900

    interface review

--- src/ch6/Abstract.java
+++ src/ch6/Abstract.java
@@ -10,50 +10,123 @@ public class Abstract {
         unitArr[0] = new Marine();
         unitArr[1] = new Tank();
         unitArr[2] = new DropShip();
-        Arrays.stream(unitArr).forEach((value) -> value.move());
+
+        //Repair
+        for (Unit unit : unitArr) {
+            if (unit instanceof Repair) {
+                Repair repair = (Repair) unit;
+                while (unit.hp < 100) {
+                    ++unit.hp;
+                }
+                repair.repair();
+            }
+            unit.move();
+            unit.stop();
+        }
+
     }
 }
-abstract class Unit{
-    int x , y;
 
-    Unit(int x,int y){
+abstract class Unit {
+    int x, y, hp;
+
+    Unit() {
+
+    }
+
+    Unit(int x, int y, int hp) {
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+    }
+
+    abstract void move();
+
+    void stop() {
+        System.out.println("멈추다");
+    }
+
+}
+
+abstract class GroundUnit extends Unit {
+    GroundUnit() {
+
+    }
+
+    GroundUnit(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    abstract void move();
-    void stop(){
-        System.out.println("멈추다");
-    };
+    GroundUnit(int x, int y, int hp) {
+        super(x, y, hp);
+    }
 }
 
-class Marine extends Unit{
-    Marine(){
-        super(100,100);
+abstract class AirUnit extends Unit {
+    AirUnit() {
+
     }
+
+    AirUnit(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    AirUnit(int x, int y, int hp) {
+        super(x, y, hp);
+    }
+}
+
+interface Repair {
+    void repair();
+}
+
+class Marine extends GroundUnit {
+    Marine() {
+        super(100, 100);
+    }
+
     @Override
     void move() {
         System.out.println("마린이 움직임 " + x + y);
     }
+
 }
 
-class Tank extends Unit{
-    Tank(){
-        super(200,100);
+class Tank extends GroundUnit implements Repair {
+    Tank() {
+        super(200, 100, 50);
     }
+
     @Override
     void move() {
         System.out.println("탱크가 움직임 " + x + y);
     }
+
+    @Override
+    public void repair() {
+        System.out.println("탱크의 수리가 완료되었습니다.");
+    }
+
 }
 
-class DropShip extends Unit{
-    DropShip(){
-        super(200,100);
+class DropShip extends AirUnit implements Repair {
+    DropShip() {
+        super(200, 100, 50);
     }
+
     @Override
     void move() {
         System.out.println("드랍쉽이 움직임 " + x + y);
     }
+
+    @Override
+    public void repair() {
+        System.out.println("드랍쉽의 수리가 완료되었습니다.");
+    }
 }
+
+
+
 
