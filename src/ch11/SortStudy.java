a6aabcc7bbc435e7b72d12fcea772219ea6ce1b5
ats3059 <ats3059@naver.com>
2021-12-27 23:46:01 +0900

    comparable , comparator study

---
+++ src/ch11/SortStudy.java
@@ -0,0 +1,47 @@
+package ch11;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortStudy {
+
+    public static void main(String[] args) {
+        int[] intArr = {6,0,3,8,7,1};
+
+        for(int i = 0; i < intArr.length-1; i++){
+            for(int j = 0; j < intArr.length-1-i; j++){
+                int sw = 0;
+                if(intArr[j] > intArr[j+1]){
+                    sw = intArr[j+1];
+                    intArr[j+1] = intArr[j];
+                    intArr[j] = sw;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(intArr));
+
+
+        String[] str = {"hello","ab","cd","AB","CD"};
+        Arrays.sort(str);
+        System.out.println(Arrays.toString(str));
+        Arrays.sort(str,String.CASE_INSENSITIVE_ORDER);
+        System.out.println(Arrays.toString(str));
+        Arrays.sort(str , new Reverse());
+        System.out.println(Arrays.toString(str));
+    }
+
+}
+class Reverse implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        if(o1 instanceof Comparable && o2 instanceof Comparable){
+            Comparable co1 = (Comparable)o1;
+            Comparable co2 = (Comparable)o2;
+
+            return co1.compareTo(co2) * -1;
+        }
+
+        return -1;
+    }
+}
