743abbfa1a33092aff1fb13beaf610d885ef2f30
ats3059 <ats3059@naver.com>
2022-01-25 01:27:07 +0900

    modfiy

--- src/ch13/Thread6.java
+++ src/ch13/Thread6.java
@@ -1,43 +1,51 @@
 package ch13;
 
-public class Thread6 extends Thread{
-    int stock = 1000;
 
-    public static void main(String[] args) {
-        Thread thread1 = new Thread6();
-
-        new Thread(thread1).start();
-        new Thread(thread1).start();
-
-    }
-
-
+public class Thread6 implements Runnable {
+    Account account = new Account();
 
     @Override
     public void run() {
-       while(stock > 0){
-           stock = moneyStock(outMoney(),stock);
-       }
-    }
-
-    int outMoney(){
-        int out = (int)(Math.random() * 3 + 1) * 100;
-        return out;
-    }
-
-    int moneyStock(int money , int stock){
-        if(stock >= money){
-            try{
-                stock = stock - money;
-                System.out.println(stock);
-                Thread.sleep(1000);
-            }catch (InterruptedException e) {
-
-            }
-        }else{
-            System.out.println("잔고부족");
+        while (account.getStock() > 0) {
+            account.moneyStock(account.outMoney(), account.getStock());
         }
+    }
+
+}
+
+class Start {
+    public static void main(String[] args) {
+        Runnable r = new Thread6();
+        new Thread(r).start();
+        new Thread(r).start();
+    }
+
+}
+
+class Account {
+    private int stock = 1000;
+
+    public synchronized int getStock() {
         return stock;
     }
 
+    int outMoney() {
+        int out = (int) (Math.random() * 3 + 1) * 100;
+        return out;
+    }
+
+    synchronized int moneyStock(int money, int stock) {
+        if (stock >= money) {
+            try {
+                this.stock = stock - money;
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+            System.out.println(stock + " " + Thread.currentThread().getName());
+        } else {
+            System.out.println("잔고부족" + " " + stock + " " + Thread.currentThread().getName());
+        }
+        return stock;
+    }
 }
