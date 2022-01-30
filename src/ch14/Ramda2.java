d6dabf4f5fc4ab9c2f774cd1a70d85a94deb77cb
ats3059 <ats3059@naver.com>
2022-01-30 22:38:20 +0900

    modfiy

--- src/ch14/Ramda2.java
+++ src/ch14/Ramda2.java
@@ -4,7 +4,23 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Ramda2 {
+    /*
+        Predicate의 결합
 
+        and(),or(),negate()로 두 Predicate를 하나로 결합(default 메서드)
+
+
+    Function의 결합도 가능하다
+        Function<Integer,String> f = s -> Integer.parseInt(s,16);
+        Function<String,Integer> g = i -> Integer.toBinaryString(i);
+
+        s.andThen(g) // s함수 실행 이후 g함수 실행
+        s.compose(g) // g함수 시행 이후 s함수 실행 -> 걍 g.andThen(s)사용하는게 보기가 더 편하다...
+
+    컬렉션 프레임웍과 함수형 인터페이스
+	
+
+     */
     public static void main(String[] args) {
         Function<String,Integer> hexConvert = i -> Integer.parseInt(i,16);
         Function<Integer,String> binaryConvert = i -> Integer.toBinaryString(i);
