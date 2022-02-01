91a7d4a30a43ec95779c1f47f31f71e3037d6009
ats3059 <ats3059@naver.com>
2022-02-01 23:32:18 +0900

    modify

--- src/ch14/Ramda3.java
+++ src/ch14/Ramda3.java
@@ -1,12 +1,21 @@
 package ch14;
 
-import java.util.function.Function;
 
 public class Ramda3 {
+/*
+서드 참조
+	하나의 메서드만 호출하는 람다식은 '메서드 참조'로 간단히 할 수 있다.
 
+생성자의 메서드 참조
+
+	Supplier<MyClass> s = () -> new MyClass();
+
+
+ */
     public static void main(String[] args) {
         //익명객체이다 -> 익명클래스의 제약조건과 같음 // 사실상 내부클래스임..
         TripleRamda<String,Integer,String,TestClass> tripleRamda = TestClass::new;
+
         System.out.println(tripleRamda);
         TestClass testClass = tripleRamda.method("name", 13, "seoul");
         System.out.println(testClass);
--- src/ch14/TripleRamda.java
+++ src/ch14/TripleRamda.java
@@ -1,5 +1,8 @@
 package ch14;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @FunctionalInterface
 public interface TripleRamda<A,S,F,R> {
     R method(A a,S s, F f);
