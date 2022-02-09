9b184762f5a58d763346896f15999d650112ed09
ats3059 <ats3059@naver.com>
2022-02-09 21:49:46 +0900

    Optional study

---
+++ src/ch14/OptionalStudy.java
@@ -0,0 +1,35 @@
+package ch14;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class OptionalStudy {
+    public static void main(String[] args) {
+        String str = "1234";
+        Optional<String> optional = Optional.of(str);
+        //Optional 의 값을 가져온다
+        optional.ifPresent(System.out::println);
+
+        Optional<String> optional1 = Optional.of("456789");
+        Integer integer = optional1.map(Integer::parseInt).orElseThrow(NoSuchElementException::new);
+        System.out.println(integer);
+
+        OptionalInt optionalInt = OptionalInt.of(0);
+        OptionalInt optionalInt2 = OptionalInt.empty();
+
+        // isPresent() -> null이면 false null이 아니면 true
+        //null 아님.(empty 가 아니니까..) true
+        System.out.println(optionalInt.isPresent());
+        System.out.println(optionalInt.getAsInt());
+
+        // false empty
+        System.out.println(optionalInt2.isPresent());
+
+
+
+
+
+
+    }
+}
