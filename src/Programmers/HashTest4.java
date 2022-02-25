de516f4a0915e8c2a460e3661ca7c3f5648aee4a
ats3059 <ats3059@naver.com>
2022-02-26 01:10:23 +0900

    Hash Study

--- src/Programmers/HashTest4.java
+++ src/Programmers/HashTest4.java
@@ -19,6 +19,31 @@ public class HashTest4 {
 
     int solution(String s1 , String s2){
        int answer = 0;
+       Map<Character,Integer> amap = new HashMap<>();
+       Map<Character,Integer> bmap = new HashMap<>();
+       int len = s1.length();
+       int len2 = s2.length()-1;
+       int lt = 0;
+
+       for(char c : s2.toCharArray()){
+           bmap.put(c,bmap.getOrDefault(c,0)+1);
+       }
+
+       for(int i = 0; i < len2;i++){
+           amap.put(s1.charAt(i),amap.getOrDefault(s1.charAt(i),0)+1);
+       }
+
+       for(int rt = len2; rt < len;rt++){
+           amap.put(s1.charAt(rt),amap.getOrDefault(s1.charAt(rt),0)+1);
+           if(amap.equals(bmap)) answer++;
+           amap.put(s1.charAt(lt),amap.get(s1.charAt(lt))-1);
+           if(amap.get(s1.charAt(lt))==0) amap.remove(s1.charAt(lt));
+           lt++;
+
+       }
+
+
+
 
 
 
