b60a24a7ab33ef1f55dad534a74b4170a873aaed
ats3059 <ats3059@naver.com>
2022-08-06 23:16:43 +0900

    프로그래머스 LV2

---
+++ src/Programmers/Camouflage.java
@@ -0,0 +1,42 @@
+package Programmers;
+
+import java.util.*;
+
+public class Camouflage {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        Map<String,List<String>> map = new HashMap<>();
+        int size = clothes.length;
+
+        for(int i = 0; i < size; i++){
+            String name = clothes[i][0];
+            String type = clothes[i][1];
+            if(!map.containsKey(type)){
+                List<String> list = new ArrayList<>();
+                list.add(name);
+                map.put(type,list);
+
+            }else{
+                map.get(type).add(name);
+            }
+        }
+
+
+
+        for(String s : map.keySet()){
+            int value = map.get(s).size();
+            value++;
+            if(answer == 0) answer = value;
+            else answer *= value;
+        }
+
+        answer--;
+
+
+
+
+
+
+        return answer;
+    }
+}
