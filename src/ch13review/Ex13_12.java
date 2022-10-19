5ec3c0dd9ea152d53f8c31d742848848489dcdf6
ats3059 <ats3059@naver.com>
2022-10-19 22:56:30 +0900

    synchronized 복습

---
+++ src/ch13review/Ex13_12.java
@@ -0,0 +1,53 @@
+package ch13review;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class Ex13_12 {
+
+    public static void main(String[] args) {
+        Runnable r = new RunnableEx12();
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+
+        t1.start();
+        t2.start();
+    }
+
+}
+
+class Account{
+    private int balance = 1000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public synchronized void withdraw(int money){
+        if(balance >= money){
+            sleep();
+            balance -= money;
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
+
+class RunnableEx12 implements Runnable {
+    Account acc = new Account();
+    @Override
+    public void run() {
+        while (acc.getBalance() > 0) {
+            int money = (int)(Math.random() * 3 + 1) * 100;
+            acc.withdraw(money);
+            System.out.println("balance : " + acc.getBalance());
+        }
+    }
+}
+
